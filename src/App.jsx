5568a384992cfb59b5cd5233deb06f13626235cf
yeeun <jovm268@naver.com>
2025-08-18 18:31:22 +0900

    [feat] advanced todo 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Advice from "./components/Advice";
@@ -27,47 +27,83 @@ const mok = [
   },
 ];
 
+//reducer함수 만들기 (todo의 상태 변화를 담당)
+function reducer(state, action) {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      //새로운 할일을 추가하면, 기존의 데이터와 스프레드배열로 state상태값으로 함께 조인
+      return [action.data, ...state];
+    }
+    case "UPDATE": {
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    }
+    case "DELETE": {
+      return state.filter((item) => item.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, content: action.newContent }
+          : item
+      );
+    }
+    default: {
+      return state;
+    }
+  }
+}
 function App() {
-  const [todo, setTodo] = useState(mok);
+  const [todo, dispatch] = useReducer(reducer, mok);
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "INIT", data });
+      });
+  }, []);
   const onCreate = (content) => {
     const newTodo = {
-      //json서버 파일을 보낼때, ID값은 자동으로 만들어줌
+      id: uuidv4(),
       isDone: false,
       content: content,
       date: new Date().getTime(),
     };
-    //서버로 보내주기
     fetch("http://localhost:3000/todo", {
       method: "POST",
       body: JSON.stringify(newTodo),
+      headers: { "Content-Type": "application/json" },
     })
       .then((res) => res.json())
-      .then((res) => setTodo([...todo, res]));
+      .then((data) => dispatch({ type: "CREATE", data }));
   };
 
   //체크 박스 선택하면 토글 해주는 메서드
   const onUpdate = (targetId) => {
-    //체크박스가 선택된 아이디 값 받아와서, todo의 상태값과 동일한 Id를 갖는다?
-    //그럼 todo의 상태 변화 시켜
-    setTodo(
-      todo.map((todos) =>
-        todos.id === targetId ? { ...todos, isDone: !todos.isDone } : todos
-      )
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   //삭제하는 로직
   const onDelete = (targetId) => {
-    setTodo(todo.filter((todos) => todos.id !== targetId));
+    dispatch({ type: "DELETE", targetId: targetId });
   };
 
   //수정버튼 기능
   const onEdit = (targetId, newContent) => {
-    setTodo(
-      todo.map((todos) =>
-        todos.id === targetId ? { ...todos, content: newContent } : todos
-      )
-    );
+    fetch(`http://localhost:3000/todos/${encodeURIComponent(targetId)}`, {
+      method: "PATCH", // 기존 데이터 수정
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: newContent }),
+    })
+      .then((res) => res.json())
+      .then(() => dispatch({ type: "EDIT", targetId, newContent }));
   };
 
   return (
